8e92438e5f2123e3fdebb11c9da8cb55c5ad11a5
Gaziz Tuleukulov <gaziz.cyprus@gmail.com>
2021-11-12 12:10:37 +0500

    feat add view pug

--- server.js
+++ server.js
@@ -6,14 +6,14 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
 
-
+app.set('view engine', 'pug')
 fccTesting(app); //For FCC testing purposes test
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.route('/').get((req, res) => {
-  res.render('Load your view here');
+  res.render(process.cws() + '/views/pug');
 });
 
 const PORT = process.env.PORT || 3000;
